555db3c2f25a3e0b580fb916da1adc23dc88b558
rt.wadewang <wadewang@rayootech.com>
2022-03-25 08:55:46 +0800

    统一view项目目录

--- autotest/src/phpunit.sh
+++ autotest/src/phpunit.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-sed -i "9c define('TK_PATH', '/var/tke/dev2');" /var/tke/dev2/autotest/phpunit/library/BaseBootstrap.php
-sed -i "23c \$_SERVER['DOCUMENT_ROOT'] = '/var/tke/dev2/core/web';" /var/tke/dev2/autotest/phpunit/library/BaseBootstrap.php
-tar -xzvf /var/data/phpunit.tar.gz -C /var/tke/dev2/autotest/phpunit
+sed -i "9c define('TK_PATH', '/home/tke/dev2');" /home/tke/dev2/autotest/phpunit/library/BaseBootstrap.php
+sed -i "23c \$_SERVER['DOCUMENT_ROOT'] = '/home/tke/dev2/core/web';" /home/tke/dev2/autotest/phpunit/library/BaseBootstrap.php
+tar -xzvf /var/data/phpunit.tar.gz -C /home/tke/dev2/autotest/phpunit
+chmod -R 755 /home/tke/dev2/autotest/phpunit/vendor/phpunit/phpunit/phpunit
 echo "success!"
