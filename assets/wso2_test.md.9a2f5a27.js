fe0df95c9a02f59e3c150629599bccf8b5f2f0ff
wqf100124 <wqf100124@users.noreply.github.com>
2022-11-18 05:46:26 +0000

    deploy: 40bbfae8b35c37ae81f7d135655370b839c1b578

---
+++ assets/wso2_test.md.9a2f5a27.js
@@ -0,0 +1,17 @@
+import{_ as s,c as a,o as e,a as n}from"./app.339c1650.js";const p="/tke-view/image/screenshots/wso2/deploy/5.png",k=JSON.parse('{"title":"\u6D4B\u8BD5","description":"","frontmatter":{},"headers":[{"level":2,"title":"Dev\u73AF\u5883","slug":"dev\u73AF\u5883","link":"#dev\u73AF\u5883","children":[]},{"level":2,"title":"live\u73AF\u5883","slug":"live\u73AF\u5883","link":"#live\u73AF\u5883","children":[]}],"relativePath":"wso2/test.md"}'),t={name:"wso2/test.md"},l=n(`<h1 id="\u6D4B\u8BD5" tabindex="-1">\u6D4B\u8BD5 <a class="header-anchor" href="#\u6D4B\u8BD5" aria-hidden="true">#</a></h1><h2 id="dev\u73AF\u5883" tabindex="-1">Dev\u73AF\u5883 <a class="header-anchor" href="#dev\u73AF\u5883" aria-hidden="true">#</a></h2><div class="language-ini"><button title="Copy Code" class="copy"></button><span class="lang">ini</span><pre class="shiki"><code><span class="line"><span style="color:#676E95;"># AP	</span></span>
+<span class="line"><span style="color:#A6ACCD;">https://apiapdev.fos.tkeasia.com</span></span>
+<span class="line"><span style="color:#676E95;"># EA</span></span>
+<span class="line"><span style="color:#A6ACCD;">https://apieadev.tkeview.com</span></span>
+<span class="line"><span style="color:#676E95;"># NA</span></span>
+<span class="line"><span style="color:#A6ACCD;">https://apinadev.tkeview.com</span></span>
+<span class="line"><span style="color:#676E95;"># SA</span></span>
+<span class="line"><span style="color:#A6ACCD;">https://apisadev.tkeview.com</span></span>
+<span class="line"></span></code></pre></div><h2 id="live\u73AF\u5883" tabindex="-1">live\u73AF\u5883 <a class="header-anchor" href="#live\u73AF\u5883" aria-hidden="true">#</a></h2><div class="language-ini"><button title="Copy Code" class="copy"></button><span class="lang">ini</span><pre class="shiki"><code><span class="line"><span style="color:#676E95;"># AP	</span></span>
+<span class="line"><span style="color:#A6ACCD;">https://apiap.fos.tkeasia.com</span></span>
+<span class="line"><span style="color:#676E95;"># EA</span></span>
+<span class="line"><span style="color:#A6ACCD;">https://apiea.tkeview.com</span></span>
+<span class="line"><span style="color:#676E95;"># NA</span></span>
+<span class="line"><span style="color:#A6ACCD;">https://apina.tkeview.com</span></span>
+<span class="line"><span style="color:#676E95;"># SA</span></span>
+<span class="line"><span style="color:#A6ACCD;">https://apisa.tkeview.com</span></span>
+<span class="line"></span></code></pre></div><p>\u4EE5AP\u56FD\u5BB6\u4E3A\u4F8B:</p><p>\u83B7\u53D6Token: <a href="https://apiapdev.fos.tkeasia.com/token" target="_blank" rel="noreferrer">https://apiapdev.fos.tkeasia.com/token</a> <br> API\u63A5\u53E3: <a href="https://apiapdev.fos.tkeasia.com" target="_blank" rel="noreferrer">https://apiapdev.fos.tkeasia.com</a></p><p><img src="`+p+'" alt=""></p>',8),o=[l];function i(c,r,d,h,v,_){return e(),a("div",null,o)}const m=s(t,[["render",i]]);export{k as __pageData,m as default};
