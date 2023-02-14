4994f5e317d837365da21a6a43afe3c9cc7640b9
rt.wadewang <wadewang@rayootech.com>
2023-02-14 13:41:30 +0800

    修改autotest项目路径

--- autotest/src/phpunit.sh
+++ autotest/src/phpunit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sed -i "9c define('TK_PATH', '/home/tke/code');" /home/tke/code/autotest/phpunit/library/BaseBootstrap.php
-sed -i "23c \$_SERVER['DOCUMENT_ROOT'] = '/home/tke/code/web';" /home/tke/code/autotest/phpunit/library/BaseBootstrap.php
+sed -i "9c define('TK_PATH', '/home/tke/core');" /home/tke/autotest/phpunit/library/BaseBootstrap.php
+sed -i "23c \$_SERVER['DOCUMENT_ROOT'] = '/home/tke/core/web';" /home/tke/autotest/phpunit/library/BaseBootstrap.php
 echo "success!"
