2868573b960730da8d6e5235fa1b5f18a1a95cbb
wqf100124 <wqf100124@users.noreply.github.com>
2023-02-15 22:49:55 +0000

    deploy: ba0f109f2c6d2504cb0070db58f991a21126e570

---
+++ assets/wso2_test.md.81215cb2.js
@@ -0,0 +1,17 @@
+import{_ as s,c as a,o as e,a as n}from"./app.0ff1bfb5.js";const t="/tke-view/image/screenshots/wso2/deploy/5.png",A=JSON.parse('{"title":"测试","description":"","frontmatter":{},"headers":[{"level":2,"title":"Dev环境","slug":"dev环境","link":"#dev环境","children":[]},{"level":2,"title":"live环境","slug":"live环境","link":"#live环境","children":[]}],"relativePath":"wso2/test.md"}'),l={name:"wso2/test.md"},p=n(`<h1 id="测试" tabindex="-1">测试 <a class="header-anchor" href="#测试" aria-hidden="true">#</a></h1><h2 id="dev环境" tabindex="-1">Dev环境 <a class="header-anchor" href="#dev环境" aria-hidden="true">#</a></h2><div class="language-ini"><button title="Copy Code" class="copy"></button><span class="lang">ini</span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#676E95;font-style:italic;"># AP	</span></span>
+<span class="line"><span style="color:#A6ACCD;">https://apiapdev.fos.tkeasia.com</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"># EA</span></span>
+<span class="line"><span style="color:#A6ACCD;">https://apieadev.tkeview.com</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"># NA</span></span>
+<span class="line"><span style="color:#A6ACCD;">https://apinadev.tkeview.com</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"># SA</span></span>
+<span class="line"><span style="color:#A6ACCD;">https://apisadev.tkeview.com</span></span>
+<span class="line"></span></code></pre></div><h2 id="live环境" tabindex="-1">live环境 <a class="header-anchor" href="#live环境" aria-hidden="true">#</a></h2><div class="language-ini"><button title="Copy Code" class="copy"></button><span class="lang">ini</span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#676E95;font-style:italic;"># AP	</span></span>
+<span class="line"><span style="color:#A6ACCD;">https://apiap.fos.tkeasia.com</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"># EA</span></span>
+<span class="line"><span style="color:#A6ACCD;">https://apiea.tkeview.com</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"># NA</span></span>
+<span class="line"><span style="color:#A6ACCD;">https://apina.tkeview.com</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"># SA</span></span>
+<span class="line"><span style="color:#A6ACCD;">https://apisa.tkeview.com</span></span>
+<span class="line"></span></code></pre></div><p>以AP国家为例:</p><p>获取Token: <a href="https://apiapdev.fos.tkeasia.com/token" target="_blank" rel="noreferrer">https://apiapdev.fos.tkeasia.com/token</a> <br> API接口: <a href="https://apiapdev.fos.tkeasia.com" target="_blank" rel="noreferrer">https://apiapdev.fos.tkeasia.com</a></p><p><img src="`+t+'" alt=""></p>',8),i=[p];function o(c,r,d,h,v,_){return e(),a("div",null,i)}const f=s(l,[["render",o]]);export{A as __pageData,f as default};
