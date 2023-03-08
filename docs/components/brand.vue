1e12027e2d9b3d8dc94838547789e75d44a66638
rt.wadewang <wadewang@rayootech.com>
2023-03-08 13:14:34 +0800

    添加动画文字

--- docs/components/brand.vue
+++ docs/components/brand.vue
@@ -1,10 +1,91 @@
 <template>
-  <view class="brand">
-    brand
+  <view v-show="visible">
+    <div id="chart" style="width: 100%; height: 80px;margin: 0 64px;"></div>
   </view>
 </template>
 
 <script setup>
+import * as echarts from "echarts";
+import {onMounted, defineProps, ref, reactive} from "vue";
+
+const props = defineProps({
+  text: {
+    required: true,
+    type: String
+  }
+});
+
+const visible = ref(true);
+const option = reactive({
+  graphic: {
+    elements: [
+      {
+        type: 'text',
+        left: 'left',
+        top: 'center',
+        style: {
+          text: props.text,
+          fontSize: 36,
+          fontWeight: 'normal',
+          lineDash: [0, 200],
+          lineDashOffset: 0,
+          fill: 'transparent',
+          stroke: '#f35383',
+          lineWidth: 1
+        },
+        keyframeAnimation: {
+          duration: 3500,
+          loop: false,
+          keyframes: [
+            {
+              percent: 0.6,
+              style: {
+                fill: 'transparent',
+                lineDashOffset: 200,
+                lineDash: [200, 0]
+              }
+            },
+            {
+              // Stop for a while.
+              percent: 0.8,
+              style: {
+                fill: 'transparent'
+              }
+            },
+            {
+              percent: 1,
+              style: {
+                fill: '#f35383'
+              }
+            }
+          ]
+        }
+      }
+    ]
+  }
+});
+
+const myChart = ref(null);
+
+const echartInit = () => {
+  myChart.value = echarts.init(document.getElementById("chart"));
+  updateChart();
+};
+
+const updateChart = () => {
+  visible.value = document.documentElement.clientWidth > 960;
+  if (visible.value) {
+    myChart.value.setOption(option);
+  }
+}
+
+onMounted(() => {
+  echartInit();
+
+  window.onresize = () => {
+    updateChart();
+  };
+});
 </script>
 
 <style scoped>
--- docs/index.md
+++ docs/index.md
@@ -17,15 +17,11 @@ hero:
       text: 配置 Docker 环境
       link: /docker/setup
 
-features:
-  - icon: 💯
-    title: 环境一致
-    details: 参照View生产环境构建，确保开发和生产环境的一致性，支持运行Local/Dev/Dev2/RC/Live的代码。
-  - icon: ⚡️
-    title: 简洁易用
-    details: 无需繁琐的环境配置和安装流程，仅需要几条命令便可轻松搞定。
-  - icon: 🏄‍
-    title: 轻松扩展
-    details: 可集成RabbitMQ/Autotest/WSO2/Solr等开发中常用的第三方服务。
+
 ---
 
+<Brand text="宝剑锋从磨砺出，梅花香自苦寒来。"></Brand>
+
+<script setup>
+import Brand from './components/brand.vue';
+</script>
