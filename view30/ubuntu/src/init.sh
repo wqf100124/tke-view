8ba428e3b03728850c0cde0d783fc763a95036b8
rt.wadewang <wadewang@rayootech.com>
2024-11-29 14:17:48 +0800

    修改view镜像脚本

--- view30/ubuntu/src/init.sh
+++ view30/ubuntu/src/init.sh
@@ -1,67 +1,46 @@
 #!/bin/sh
-# /run/init.sh 80000110
 
-if [ ! $1 ]; then
-    echo '缺少8ID参数';
+if [ ! "$1" ]; then
+    echo '缺少8ID参数，例如: /run/init.sh 80000110';
     exit 1;
 fi
 
 corePath=/opt/tk
-userId=${1}
+user8ID=${1}
 
 if [ ! "$(ls -A $corePath)" ]; then
     echo "${corePath}目录不存在，请确认是否已经映射代码到容器中";
     exit 1;
 fi
 
-configCode='$sPreUrl = substr($_SERVER["HTTP_HOST"], 0, strpos($_SERVER["HTTP_HOST"], "."));\n    $sPreUrl = $sPreUrl ?: "hk";\n    require_once(BASE_DIR . "\/..\/sites\/" . $sPreUrl . "\/config.php");'
+cd ${corePath} || exit
 
 # tke_config.php
-if [ $1 = 'local' ]
-then
-  sed -i 's/\.\/".$sPreUrl/\.\/sites\/".$sPreUrl/' ${corePath}/sys/includes/tke_config.php
-else
-  sed -i "s/require_once(BASE_DIR . \"\/config.php\");/${configCode}/g" ${corePath}/sys/includes/tke_config.php
-fi
-sed -i '$s/?>/''/' ${corePath}/sys/includes/tke_config.php
-echo "update ${corePath}/sys/includes/tke_config.php";
+configCode='$sPreUrl = substr($_SERVER["HTTP_HOST"], 0, strpos($_SERVER["HTTP_HOST"], "."));\n    $sPreUrl = $sPreUrl ?: "hk";\n    require_once(BASE_DIR . "\/..\/sites\/" . $sPreUrl . "\/config.php");'
+sed -i "s/require_once(BASE_DIR . \"\/config.php\");/${configCode}/g" ${corePath}/sys/includes/tke_config.php
+sed -i '$s/?>/''/' sys/includes/tke_config.php
+printf "\033[32m Updated sys/includes/tke_config.php \033[0m\n"
 
-# vivid/bootstrap/app.php
-sed -i "s/require_once(BASE_DIR . \"\/..\/\".\$sPreUrl . \"\/config.php\");/require_once(BASE_DIR . \"\/..\/sites\/\".\$sPreUrl . \"\/config.php\");/g" ${corePath}/vivid/bootstrap/app.php
-echo "update ${corePath}/vivid/bootstrap/app.php";
-
-if [ $1 != 'local' ]
-then
 # http.lib
-rm ${corePath}/sys/libs/http.lib
-echo "<?php include('./ngcore/http.lib');" >> ${corePath}/sys/libs/http.lib
-echo "update ${corePath}/sys/libs/http.lib";
+rm sys/libs/http.lib
+echo "<?php include('./ngcore/http.lib');" >> sys/libs/http.lib
+printf "\033[32m Updated sys/libs/http.lib \033[0m\n"
 
 # login.php
-loginCode="\    // Use custom 8id automatic login\n    \$user = \$db->get(\"SELECT * FROM \`user\` WHERE \`ActiveDirectoryID\` = '${userId}' LIMIT 1\");\n    userLogin(\$user->id, \$user);\n    die();"
-sed -i "/UserInformationService as UserInformationService;/a\ \n${loginCode}" ${corePath}/web/login.php
-sed -i 's/use SystemAdmin\\User\\Service\\UserInformationService as UserInformationService;/use SystemAdmin\\User\\Service\\UserInformationService as UserInformationService ;/' ${corePath}/web/login.php
-echo "update ${corePath}/web/login.php";
-fi
+loginCode="\    // Use custom 8id automatic login\n    \$user = \$db->get(\"SELECT id FROM \`user\` WHERE \`ActiveDirectoryID\` = '${user8ID}' LIMIT 1\");\n    userLogin(\$user->id);\n    die();"
+sed -i "/UserInformationService as UserInformationService;/a\ \n${loginCode}" web/login.php
+sed -i 's/use SystemAdmin\\User\\Service\\UserInformationService as UserInformationService;/use SystemAdmin\\User\\Service\\UserInformationService as UserInformationService ;/' web/login.php
+printf "\033[32m Updated web/login.php \033[0m\n"
 
-# ErrorController.php
-cp /run/init/ErrorController.php ${corePath}/web/sharp/modules/default/controllers/ErrorController.php
-echo "update ${corePath}/web/sharp/modules/default/controllers/ErrorController.php";
+# Handler.php
+sed -i "s/function render(/function renderBackup(/g" vivid/app/Exceptions/Handler.php
+printf "\033[32m Updated vivid/app/Exceptions/Handler.php \033[0m\n"
 
-# error.phtml
-cp /run/init/error.phtml ${corePath}/web/sharp/modules/default/views/scripts/error/error.phtml
-echo "update ${corePath}/web/sharp/modules/default/views/scripts/error/error.phtml";
-
-# ViewLoggerConfig.php
-cp /run/init/ViewLoggerConfig.php ${corePath}/ViewLoggerConfig.php
-echo "create ${corePath}/ViewLoggerConfig.php";
-
-# dbg.lib
-dbgCode='\n    echo <<<END\n    <div style="width: 60%;margin: 3rem auto;padding: 1rem 1.5rem;word-break: break-all;font-size: 14px;border-radius: 10px;background: #fff;box-shadow: 0px 4px 10px 1px rgb(123 123 123 / 15%);"><h1>数据库异常</h1>\n    <h3>错误代码:</h3><p>{$db->errno()}</p>\n    <h3>报错信息:</h3><p style="padding: 1rem;background-color: #fff6f6;color: #9f3a38;border-radius: 4px;">{$db->error()}</p>\n    <h3>SQL语句:</h3><pre style="word-break: break-word;white-space: break-spaces;background: #f8f8f9;padding: 1rem;border-radius: 4px;"><code>{$db->getLastSql()}</code></pre>\n    </div>\n    END;\n    die();'
-sed -i "/\$iMessageId = errorMessageInfo (\$message,\$type=DBG_USER,\$dbInfor,\$info);/a\ \n${dbgCode}" ${corePath}/sys/libs/dbg.lib
-echo "update ${corePath}/sys/libs/dbg.lib";
-
-echo "操作成功!";
-echo "警告：以上文件仅可用于本地开发，切勿提交!!!";
-echo "警告：以上文件仅可用于本地开发，切勿提交!!!";
-echo "警告：以上文件仅可用于本地开发，切勿提交!!!";
+printf "\033[32m 初始化操作完成 \033[0m\n"
+printf "\033[33m 警告，以上文件仅用于本地开发，请勿提交到Git仓库!\033[0m\n"
+cat << EOF
+可以运行以下命令来忽略这些文件的更改：
+git update-index --skip-worktree sys/includes/tke_config.ph sys/libs/http.lib web/login.php vivid/app/Exceptions/Handler.php
+如果要重新提交更改，请运行以下命令：
+git update-index --no-skip-worktree sys/includes/tke_config.ph sys/libs/http.lib web/login.php vivid/app/Exceptions/Handler.php
+EOF
