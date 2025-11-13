713f1133db4debc82c0af48fd0580cf4dc5309e5
rt.wadewang <wadewang@rayootech.com>
2025-11-13 17:46:24 +0800

    优化脚本

--- view/debian/src/init.sh
+++ view/debian/src/init.sh
@@ -27,10 +27,24 @@ echo "<?php include('./ngcore/http.lib');" >> sys/libs/http.lib
 printf "\033[32m Updated sys/libs/http.lib \033[0m\n"
 
 # login.php
-loginCode="\    // Use custom 8id automatic login\n    \$user = \$db->get(\"SELECT id FROM \`user\` WHERE \`ActiveDirectoryID\` = '${user8ID}' LIMIT 1\");\n    userLogin(\$user->id);\n    die();"
+loginCode="\    // Use custom 8id automatic login\n    \$user = \$db->get(\"SELECT id FROM \`user\` WHERE \`ActiveDirectoryID\` = '${user8ID}' LIMIT 1\");\n    if (empty(\$user))die('Incorrect 8ID.');userLogin(\$user->id);\n    die();"
 sed -i "/UserInformationService as UserInformationService;/a\ \n${loginCode}" web/login.php
 sed -i 's/use SystemAdmin\\User\\Service\\UserInformationService as UserInformationService;/use SystemAdmin\\User\\Service\\UserInformationService as UserInformationService ;/' web/login.php
 printf "\033[32m Updated web/login.php \033[0m\n"
+
+# multilingual.lib
+sed -i.bak 's#function _xlate($source,$moduleid = null,$dbx = null) {#function _xlate($source,$moduleid = null,$dbx = null) {return $source;#' sys/libs/multilingual.lib
+printf "\033[32m Updated sys/libs/multilingual.lib \033[0m\n"
+
+sed -i "s/'options' => \$redisSentinel/\/\/ 'options' => \$redisSentinel/" vivid/config/10.x/database.php
+sed -i "s/'default' => \$redisSentinel/\/\/ 'default' => \$redisSentinel/" vivid/config/10.x/database.php
+sed -i "s/'cache' => \$redisSentinel/\/\/ 'cache' => \$redisSentinel/" vivid/config/10.x/database.php
+printf "\033[32m Updated vivid/config/10.x/database.php \033[0m\n"
+
+sed -i "s/'options' => array_merge/\/\/ 'options' => array_merge/" vivid/config/12.x/database.php
+sed -i "s/'default' => \$redisSentinel/\/\/ 'default' => \$redisSentinel/" vivid/config/12.x/database.php
+sed -i "s/'cache' => \$redisSentinel/\/\/ 'cache' => \$redisSentinel/" vivid/config/12.x/database.php
+printf "\033[32m Updated vivid/config/12.x/database.php \033[0m\n"
 
 # Handler.php
 sed -i "s/function render(/function renderBackup(/g" vivid/app/Exceptions/Handler.php
